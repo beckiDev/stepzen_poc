9b65843ac7aa1ae39c2be345c4f4c30f0e6ddf21
Rebecka Arieli <rebeckaarieli@Rebeckas-MacBook-Air.local>
2020-06-23 14:30:33 -0700

    2 commit

--- server/server.js
+++ server/server.js
@@ -86,4 +86,5 @@ app.use('/graphql', cors(), express_graphql({
     rootValue: root,
     graphiql: true
 }));
+
 app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
